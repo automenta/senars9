e0e5c9d7113584e28586488f80cbc498f831d12a
me <me@internet.earth>
2025-10-14 12:22:55 -0400

    cleanup

--- jest.config.js
+++ jest.config.js
@@ -6,11 +6,12 @@ export default {
   testPathIgnorePatterns: [
     '/tests/experimental/',
     '/tests/experimental2/',
+    '/tests/experimental3/',
     '/ui/'
   ],
   testMatch: ['**/*.test.js', '**/*.spec.js', '**/*.test.mjs', '**/*.spec.mjs'],
   // Serial execution to prevent resource contention during System initialization
-  maxWorkers: 1, // Single worker forces serial execution
+  //maxWorkers: 1, // Single worker forces serial execution
   testTimeout: 10000, // Increase default test timeout to 15 seconds
   setupFilesAfterEnv: ['<rootDir>/tests/test-setup.js'], // Add test setup file
   // Force exit after tests complete to handle any remaining async operations
